185bb9e2832d6a98a41212141ac6fd9e5782d72c
Jérémie Schnell <jeremie.schnell@hublo.com>
2023-09-07 14:29:22 +0200

    v1

--- cypress/examples/hublo_interview_example_1/cypress/e2e/js_hublo_interview_example_1.cy.js
+++ cypress/examples/hublo_interview_example_1/cypress/e2e/js_hublo_interview_example_1.cy.js
@@ -4,16 +4,7 @@
 // Hi and welcome to Hublo cypress test
 // We are going to validates a few things:
 
-// 1. Testing error message, if we send incorrect values, "Username and/or password is incorrect" should be visible in red.
-// 2. Testing unauthorized page, user should see "You are not logged in and cannot access this page" if he's not logged
-// 3. Testing login, ensure input are displayed and put correct credentials, when click on login, user should be redirected to /dashboard page
-// 4. Whitout keeping previous context, connect using request cypress API
-// 5. Finally, create a command for login in and call it in a test to ensure page "Users" is accessible when connected
-// 6.a Low level Algorythm: method get_table is returning a table with 50 values between 1 and 10, please calculate the sum of numbers between or equal to 4 and 7
-// 6.b Middle level Algprythm: method get_table is returning a table with 50 values between 1 and 10, please determine if there is more 4 than 7
-// 6.c High level Algprythm: method get_table is returning a table with 50 values between 1 and 10, please create a method to define which number are present a odd number of times
 
-//
 // REMINDER
 // Be sure to to start the server
 // before running the tests
